a026e2d132d5e4e65f7eccdc4b34edcf9e448518
brahim nouar <brahimnouar4@gmail.com>
2024-06-10 16:57:56 +0100

    first commit

---
+++ try and cath .dart
@@ -0,0 +1,65 @@
+void main(){
+// ~/ ====== int 
+// /  ====== double
+
+// try{
+//     // كود في حالة خطر
+// }catch("name of parametar"){
+//     print("order $name of paremeatr ");
+// }
+
+int x = 10 ;
+int z = 0 ;
+
+try{
+    int v = x ~/ z ; 
+    print(v);
+}catch(q){
+    print("there is a erore : $q");
+}
+// output =====> there is a erore : IntegerDivisionByZeroException.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
